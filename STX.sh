e5f3a99de8b93289145d88b67180446d995e7937
Kristina Black <115893382+kristinaleilani@users.noreply.github.com>
2023-07-29 14:59:27 -0500

    Update STX.sh

--- STX.sh
+++ STX.sh
@@ -435,9 +435,19 @@ echo 'for K in `seq 6` ; do  NGSadmix -likes STX2.beagle.gz -K $K -P 12 -o STX${
 ls6_launcher_creator.py -j adm -n adm -a IBN21018 -e kblack@utexas.edu -t 1:00:00 -w 1 
 sbatch adm.slurm
 
+
+# Rerunning angsd one more time to produce a glf file for ngsRelate (must use -doGlf 3)
+export MinIndPerc=0.75
+FILTERS1='-minInd $MI2 -uniqueOnly 1 -remove_bads 1 -minMapQ 20 -minQ 20 -snp_pval 1e-5 -minMaf 0.05 -dosnpstat 1 -doHWE 1 -hetbias_pval 1e-3 -skipTriallelic 1'
+TODO1='-doMajorMinor 1 -doMaf 1 -doCounts 1 -doPost 1 -doGlf 3'
+echo 'export NIND=`cat bams.qc | wc -l`; export MI=`echo "($NIND*$MinIndPerc+0.5)/1" | bc`' >calc3
+echo "source calc3 && angsd -b bams.qc -GL 1 $FILTERS1 $TODO1 -P 12 -out STX3">g3
+ls6_launcher_creator.py -j g3 -n g3 -a IBN21018 -e kblack@utexas.edu -t 2:00:00 -w 1 
+sbatch g3.slurm
+
 # Relatedness with ngsRelate
 echo 'export NIND2=`cat bams.qc | wc -l`; export NS=`zcat STX1.mafs.gz | wc -l`' >calc
-echo 'source calc && zcat STX1.mafs.gz | cut -f5 |sed 1d >freq && ngsRelate  -g STX1.glf.gz -n $NIND2 -f freq -O STX.res >STX.relatedness' >rel
+echo 'source calc && zcat STX3.mafs.gz | cut -f5 |sed 1d >freq && ngsRelate  -g STX3.glf.gz -n $NIND2 -f freq -O STX.res >STX.relatedness' >rel
 ls6_launcher_creator.py -j rel -n rel -a IBN21018 -e kblack@utexas.edu -t 0:30:00 -w 1
 sbatch rel.slurm
 
