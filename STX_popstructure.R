d682fefc600e08a482b45ee000f4b38c87e43044
Kristina Black <115893382+kristinaleilani@users.noreply.github.com>
2024-01-17 16:43:10 -0600

    Update STX_popstructure.R

--- STX_popstructure.R
+++ STX_popstructure.R
@@ -142,7 +142,7 @@ dev.off()
 # If it looks like you have two main admixture groups, let's save these assignments:
 npops=ncol(q2)
 cluster.admix=apply(q2[,1:npops],1,function(x) {return(paste(which(x>0.5),collapse=".")) })
-#save(cluster.admix,file=paste("Bonnet_clusters.RData",sep=""))
+#save(cluster.admix,file=paste("STX_clusters.RData",sep=""))
 
 # Let's plot a PCoA colored by two admixture groups:
 ord=capscale(IBS~1)
